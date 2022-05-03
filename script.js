b079d850775522786e22352bb0a64bdadb8ddf1e
utpalsinghjadon <futurescientist2805@gmail.com>
2022-05-03 11:26:02 +0530

    draw functionality and minor changes

--- script.js
+++ script.js
@@ -4,25 +4,34 @@ const choiceRock = document.querySelector('.choices-rock');
 const choicePaper = document.querySelector('.choices-paper');
 const choiceScissors = document.querySelector('.choices-scissors');
 const userScore = document.querySelector('.score-user');
-const userComputer = document.querySelector('.score-computer');
+const computerScore = document.querySelector('.score-computer');
 const winnerMessage = document.querySelector('.message');
+const newGameButton = document.querySelector('.newgame-button')
 
-const rock = 1;
-const paper = 2;
-const scissors = 3;
+const choices = [[1, 'Rock'], [2, 'Paper'], [3, 'Scissors']]
 
-const userMessage = (compchoice, userchoice) => {
-    winnerMessage.textContent = `You Choose ${userchoice} and Computer Choose ${compchoice}, Hence you Win😁🙌`;
-    userScore.textContent = 1;
+const drawMsg = function (choice) {
+    winnerMessage.textContent = `Draw -> Computer and You Both Choose ${choices[choice - 1][1]}`
+    computerScore.textContent = scores[1]
+    userScore.textContent = scores[0]
 }
 
-const winner = function (userChoice) {
+const winMsgUser = () => {
+    winnerMessage.textContent = "Draw"
+}
+
+let scores = [0, 0]
+
+
+const winner = function (uChoice) {
     const computerChoice = Math.ceil(Math.random() * 3)
     console.log(computerChoice)
-    if (computerChoice === 1 && userChoice === 3) {
-        userMessage(computerChoice, userChoice)
-
+    if (uChoice === computerChoice) {
+        scores[0]++
+        scores[1]++
+        drawMsg(uChoice)
     }
+
 }
 
 choiceRock.addEventListener('click', function () {
